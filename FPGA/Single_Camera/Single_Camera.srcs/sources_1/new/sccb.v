22ab8a1f2b734c16574998a61ed648ed5196d463
Gogol Bhattacharya <gogolB@users.noreply.github.com>
2018-03-17 00:09:48 -0700

    Update sccb.v
    Added Citation

--- FPGA/Single_Camera/Single_Camera.srcs/sources_1/new/sccb.v
+++ FPGA/Single_Camera/Single_Camera.srcs/sources_1/new/sccb.v
@@ -15,7 +15,7 @@
 // 
 // Revision:
 // Revision 0.01 - File Created
-// Additional Comments:
+// Additional Comments: Code from: https://embeddedmicro.com/blogs/tutorials/camera
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
